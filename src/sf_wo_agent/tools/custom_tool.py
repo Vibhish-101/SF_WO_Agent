e0ecf01246898d09afb4feed7effd2e0083a2783
Vibi-007 <vibishreddy@gmail.com>
2024-07-30 16:13:54 +0530

    change_3

--- src/sf_wo_agent/tools/custom_tool.py
+++ src/sf_wo_agent/tools/custom_tool.py
@@ -1,12 +1,77 @@
-from crewai_tools import BaseTool
+from crewai_tools import tool
+import json
 
 
-class MyCustomTool(BaseTool):
-    name: str = "Name of my tool"
-    description: str = (
-        "Clear description for what this tool is useful for, you agent will need this information to use it."
-    )
+with open('C:\Users\AD33119\OneDrive - Lumen\Documents\GitHub\SF_WO_Agent\src\sf_wo_agent\config\SF_Stub.json', 'r') as file:
+        data = json.load(file)
 
-    def _run(self, argument: str) -> str:
-        # Implementation goes here
-        return "this is an example of a tool output, ignore it and move along."
+
+@tool("Account_Status_Checker")
+def Account_Status_Checker(address):
+    "You are an expert in fetching account status for any given address."
+    acc_status = data[address]["account"]
+    return acc_status
+
+@tool("Service_Appt_Checker")
+def Service_Appt_Checker(address):
+    "You are an expert in fetching service appointment information for any given address."
+    service_appt = data[address]["service_appt"]
+    return service_appt
+
+@tool("Work_Order_Checker")
+def Work_Order_Checker(address):
+    "You are an expert in fetching work order information for any given address."
+    work_order = data[address]["work_order"]
+    return work_order
+
+@tool("Work_Order_Checker")
+def Work_Order_Checker(address):
+    "You are an expert in fetching work order information for any given address."
+    work_order = data[address]["work_order"]
+    return work_order
+
+@tool("SmartNID_Status_Checker")
+def SmartNID_Status_Checker(address):
+    "You are an expert in fetching SmartNID status for any given address."
+    smartnid_status = data[address]["smartnid status"]
+    return smartnid_status
+
+
+@tool("Adapt_Status_Checker")
+def Adapt_Status_Checker(address):
+    "You are an expert in fetching adapt status for any given address."
+    try:    
+        adapt_status = data[address]["adapt_status"]
+        return adapt_status
+    except KeyError:
+        return "Not found"
+
+
+@tool("UDIF_Checker")
+def UDIF_Checker(address):
+    "You are an expert in fetching UDIF information for any given address."
+    try:
+        result = data[address]["UDIF"]
+        return result
+    except KeyError:
+        return "Not found"
+
+
+@tool("BSU_Checker")
+def BSU_Checker(address):
+    "You are an expert in fetching BSU information for any given address."
+    try:
+        result = data[address]["BSU"]
+        return result
+    except KeyError:
+        return "Not found"
+
+
+@tool("Result_Checker")
+def Result_Checker(address):
+    "You are an expert in fetching result information for any given address."
+    result = data[address]["result"]
+    return result
+
+
+
